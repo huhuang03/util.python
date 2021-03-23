277f25a457a39cbac5e9ff0e891f64e16b2af49d
th <huhuang03@gmail.com>
2021-03-23 11:17:31 +0800

    up

--- util/jump_and_unjump.py
+++ util/jump_and_unjump.py
@@ -2,16 +2,15 @@ import os
 from .util import *
 
 def jump():
-    if os.name == 'nt':
-        # how to output the shell and let user input this shell?
+    if is_windows():
         add_to_clip_board('$Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"')
     else:
         os.system('export HTTP_PROXY="http://127.0.0.1:1080"')
         os.system('export HTTPS_PROXY="https://127.0.0.1:1080"')
 
 def unjump():
-    if os.name == 'nt':
+    if is_windows():
+        exit('unimplement in windows')
+    else:
         os.system('unset HTTP_PROXY')
         os.system('unset HTTPS_PROXY')
-    else:
-        pass
