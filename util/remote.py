8a68097037d6a5ba2546a7c08026668b9ce6fafe
huhuang03 <huhuang03@gmail.com>
2021-06-08 09:37:35 +0800

    step finish remote

--- util/remote.py
+++ util/remote.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import json
 import subprocess
 
 
@@ -7,11 +8,38 @@ def login():
     pass
 
 
+def connect(server):
+    os.system(f"ssh {server['server']}")
+
+
 def list_remotes():
-    pass
+    server_list = load_list()
+    print(server_list)
 
 
 RESERVE_NAME = []
+
+PATH_AUTHORIZED_KEYS = ".ssh/authorized_keys"
+
+PATH_SERVER_LIST = "./.server_list.txt"
+
+
+def load_list() -> [dict]:
+    if not os.path.exists(PATH_SERVER_LIST):
+        return []
+    f = open(PATH_SERVER_LIST, 'r', encoding="utf-8")
+    data = json.loads(f.read())
+    f.close()
+    if data:
+        return data
+    else:
+        return []
+
+
+def save_list(data):
+    f = open(PATH_SERVER_LIST, 'w', encoding="utf-8")
+    f.write(json.dumps(data))
+    f.close()
 
 
 def add(args):
@@ -25,20 +53,26 @@ def add(args):
     with open(local_ssh_pub, 'r') as f:
         id_rsa_text = f.read().strip()
 
-    assert(id_rsa_text)
+    assert (id_rsa_text)
 
     # ok, now let's read the rmote known_keys
     username_host = input("please input username@host: ")
-    print(f"ssh {username_host} 'cat .ssh/authorized_keys'")
-    subprocess.call(f'ssh {username_host} "cat .ssh/authorized_keys"', shell=True)
-    # os.system(f"ssh {username_host} 'cat .ssh/authorized_keys'", =True)
+    already_added = False
+    try:
+        authorized_keys = subprocess.check_output(f'ssh {username_host} "cat .ssh/authorized_keys"', shell=True,
+                                                  timeout=1)
+        authorized_keys = authorized_keys.decode("utf-8")
+        already_added = id_rsa_text in authorized_keys
+    except subprocess.SubprocessError:
+        # leave empty
+        pass
 
-    # os.system('')
-    # # how to judge already exit?
-    # # we need user input the pwd, and remember the pwd to check exists, and copy??
-    # # but I don't want the pwd. for now
-    # # so can we check can login for already exists??
-    # os.system(f"scp ~/.ssh/id_rsa.pub | ssh {username_host} 'cat >> .ssh/authorized_keys'")
+    if already_added:
+        print(f"already configured, just add the {name} into remote list")
+        save_list(load_list() + [{'name': name, 'server': username_host}])
+    else:
+        # ok, we need config the it now.
+        subprocess.run(f'cat ~/.ssh/id_rsa.pub | ssh {username_host} "cat >> {PATH_AUTHORIZED_KEYS}"')
 
 
 def copy():
@@ -50,9 +84,21 @@ def main():
     subparsers = parser.add_subparsers(dest="command")
     command_add = subparsers.add_parser("add", help="add a server")
     command_add.add_argument("name", type=str, help="a remember name, not the remote username")
+
+    subparsers.add_parser("list", help="list all server")
+
+    data = load_list()
+    for item in data:
+        subparsers.add_parser(item['name'], help=f"connect to {item['name']}[{item['server']}]")
+
     args = parser.parse_args()
+
     if args.command == "add":
         add(args)
+    elif args.command == "list":
+        list_remotes()
+    elif args.command in [item['name'] for item in data]:
+        connect([item for item in data if item['name'] == args.command][0])
 
 
 if __name__ == "__main__":
