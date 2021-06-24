9797526be70dfd676527d8111c534df9492245a3
huhuang03 <huhuang03@gmail.com>
2021-06-24 09:49:33 +0800

    up

--- util/jt_code.py
+++ util/jt_code.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import pyperclip
 import json
@@ -5,11 +7,23 @@ import json
 # IntelliJ IDEA 激活码 每日更新 长期提供【JetBrains全家桶】可用 - 白程序员的自习室
 # https://www.studytime.xin/article/code.html
 
-URL = "https://api.studytime.xin/activationCode"
+# 这个按道理说应该从公众号获取
+URL = "https://api.studytime.xin/activationCode?passwd=6247"
 
 
 def main():
-    content = requests.get(URL).content
+    os.environ['NO_PROXY'] = 'api.studytime.xin'
+
+    s = requests.Session()
+    s.headers.update({
+        "Referer": "https://www.studytime.xin/",
+        "Origin": "https://www.studytime.xin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+    })
+
+    content = s.get(URL).content
+    print(content)
     j_content = json.loads(content)
     code = j_content['data']
     print(code)
