c28f1206f0963d8b22e29714bfea9b96b82feb63
huhuang03 <huhuang03@gmail.com>
2021-07-05 10:29:24 +0800

    add local.ini

--- util/idea/idea_android.py
+++ util/idea/idea_android.py
@@ -2,11 +2,24 @@ import os
 from .idea_base import IdeaBase
 from ..app import App
 from ..env_home_win import *
+import configparser
+
+
+# how can we load the local.init
+
+def _get_android_home():
+    local_ini_path = os.path.join(os.path.dirname(__file__), "../../local.ini")
+    folder = ANDROID_HOME
+    if os.path.exists(local_ini_path):
+        config = configparser.ConfigParser()
+        config.read(local_ini_path)
+        folder = config['DEFAULT']['AS_HOME']
+    return os.path.join(folder, "bin", "studio64.exe")
 
 
 class IdeaAndroid(IdeaBase):
     def run(self, root):
-        App(os.path.join(ANDROID_HOME, "bin", "studio64.exe"), "/Applications/Android Studio.app").start()
+        App(_get_android_home(), "/Applications/Android Studio.app").start()
 
 
 def main():
