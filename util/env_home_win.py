e0a1b6128b5e4d59fe6158c7e6137ae112f70d25
huhuang03 <huhuang03@gmail.com>
2021-07-05 10:36:51 +0800

    better find android home

--- util/env_home_win.py
+++ util/env_home_win.py
@@ -1,4 +1,3 @@
-ANDROID_HOME = "E:\\Program Files\\Android\\Android Studio"
 CLION_HOME = "E:\\Program Files\\JetBrains\\CLion 2020.2.3"
 PYCHARM_HOME = "E:\\Program Files\\JetBrains\\ 2020.2.3"
 JET_BRAINS_HOME = "D:\\Program Files\\JetBrains"
--- util/idea/idea_android.py
+++ util/idea/idea_android.py
@@ -1,23 +1,32 @@
 import os
 from .idea_base import IdeaBase
 from ..app import App
-from ..env_home_win import *
 import configparser
+from util.util.util_find_program import find_program
 
-
-# how can we load the local.init
 
 def _get_android_home():
     local_ini_path = os.path.join(os.path.dirname(__file__), "../../local.ini")
-    folder = ANDROID_HOME
+    folder = ""
     if os.path.exists(local_ini_path):
         config = configparser.ConfigParser()
         config.read(local_ini_path)
         folder = config['DEFAULT']['AS_HOME']
+    else:
+        for i in range(ord('A'), ord('Z')):
+            check_folder = os.path.join(str(chr(i)), "Program Files", "Android", "Android Studio")
+            if os.path.exists(check_folder):
+                folder = check_folder
+                break
+        else:
+            exit("Can't find Android Studio installed folder")
     return os.path.join(folder, "bin", "studio64.exe")
 
 
 class IdeaAndroid(IdeaBase):
+    """
+    TODO: If not specify the android home, we need search in all driver
+    """
     def run(self, root):
         App(_get_android_home(), "/Applications/Android Studio.app").start()
 
