9cc6b908abd0f3eca55a587b448058794cba7adf
huhuang03 <huhuang03@gmail.com>
2021-07-07 10:01:29 +0800

    add no-verify in gitup

--- util/gitup.py
+++ util/gitup.py
@@ -1,9 +1,10 @@
 import os
 
+
 def main():
     os.system('git add .')
     msg = input('Please input commit message: ')
     if not msg:
         msg = 'update'
-    os.system('git commit -a -m "%s"' % (msg))
+    os.system('git commit --no-verify -a -m "%s"' % msg)
     os.system('git push')
