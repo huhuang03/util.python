f826c783522142f4045fb7098dafd670de191787
york <york@duomai.com>
2021-07-16 23:37:08 +0800

    up

--- util/an_util/wifi.py
+++ util/an_util/wifi.py
@@ -1,13 +1,18 @@
+import re
 import os
 import subprocess
 
 _PORT = 5555
+
 
 def wifi():
     """
     follow https://stackoverflow.com/questions/4893953/run-install-debug-android-applications-over-wi-fi
     adb via wifi
     """
-    ip_output = subprocess.run(["adb", "shell", "ip" "-f" "inet" "addr" "show"], stdout=subprocess.PIPE).stdout.decode("utf-8")
-    print(ip_output)
+    ip_output = subprocess.run(["adb", "shell", "ip -f inet addr show"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    ip = re.search(re.compile("inet (.*)/.*wlan0"), ip_output).group(1)
+    """this not work because will reset immediately"""
     os.system(f"adb tcpip {_PORT}")
+    os.system(f"adb connect {ip}:{_PORT}")
+    print(ip)
